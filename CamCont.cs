24dd8d385cdbb334af0f68e9d6f56ffde567a601
goktug5000 <69222008+goktug5000@users.noreply.github.com>
2022-09-23 01:56:09 +0300

    Add files via upload

---
+++ CamCont.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCont : MonoBehaviour
+{
+    public float PanSpeed=20, PanDetect, mouseX,mouseY;
+    //public float camX=0, camZ=0;
+
+    void Start()
+    {
+        Debug.Log("Screen Width : " + Screen.width);
+        Debug.Log("Screen Height : " + Screen.height);
+        PanDetect = 120;
+    }
+
+    void Update()
+    {
+        mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+        //this.gameObject.transform.position = new Vector3(camX, 30, camZ);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            PanSpeed = 50;
+        }
+        else
+        {
+            PanSpeed = 20;
+        }
+        if ((mouseX>0 && mouseX<PanDetect) || Input.GetKey(KeyCode.A))
+        {
+            //camX -= PanSpeed*Time.deltaTime;
+            transform.Translate(-PanSpeed * Time.deltaTime, 0, 0);
+        }
+        if ((mouseX > (Screen.width-PanDetect) && mouseX < Screen.width) || Input.GetKey(KeyCode.D))
+        {
+            //camX += PanSpeed*Time.deltaTime;
+            transform.Translate(PanSpeed * Time.deltaTime, 0, 0);
+        }
+        if ((mouseY > 0 && mouseY < PanDetect) || Input.GetKey(KeyCode.S))
+        {
+            //camZ -= PanSpeed * Time.deltaTime;
+            transform.Translate(0, 0, -PanSpeed * Time.deltaTime);
+        }
+        if ((mouseY > (Screen.height - PanDetect) && mouseY < Screen.height) || Input.GetKey(KeyCode.W))
+        {
+            //camZ += PanSpeed * Time.deltaTime;
+            transform.Translate(0, 0, PanSpeed * Time.deltaTime);
+        }
+        
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            this.gameObject.transform.Rotate(0, 0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            this.gameObject.transform.Rotate(0, -0.5f, 0);
+        }
+
+    }
+}
---
+++ Citizens.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Citizens : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject cube;
+    public NavMeshAgent agent;
+
+    public bool selected,deneme;
+
+    void Start()
+    {
+        
+    }
+
+
+    void movee()
+    {
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+
+            //Debug.Log(Vector3.Distance(this.gameObject.transform.position, hit.point));
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected)
+        {
+            movee();
+        }
+
+
+    }
+}
